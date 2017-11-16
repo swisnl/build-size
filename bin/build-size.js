321d04f9a40e6c68d69a7b17f321f908611461e0
Jasper Zonneveld <jasper@swis.nl>
2017-11-16 12:00:09 +0100

    Extended command documentation

--- bin/build-size.js
+++ bin/build-size.js
@@ -8,8 +8,12 @@ require('yargs')
     .command('parse', 'Parse build size of files matching provided pattern(s)', function parseCommand(yargs) {
         let argv = yargs
             .usage('Usage: $0 parse <pattern> [<pattern>] [options]')
+            .example('$0 parse ./css/*.css ./js/*.js', 'Parse multiple patterns e.g. ./css/app.css, ./js/app.js, ./js/vendor.js')
+            .example('$0 parse ./js/**/*.js', 'Parse recursive pattern e.g. ./js/app.js, ./js/vendor/vue.js')
+            .example('$0 parse ./js/*.[hash].js', 'Parse pattern with hash e.g. ./js/app.33fcb75f3ed81fc89123.js')
+            .example('$0 parse ./js/*.js > parse.json', 'Parse and store output in parse.json')
             .positional('pattern', {
-                describe: 'Glob pattern to match files',
+                describe: 'Glob pattern to match files. N.B. [hash] can be used as special matching pattern for hashed files',
                 normalize: true,
                 type: 'string'
             })
@@ -23,16 +27,18 @@ require('yargs')
         let parsed = require('../src/parse')(patterns);
         process.stdout.write(JSON.stringify(parsed, null, 2));
     })
-    .command('compare', 'Compare build sizes', function compareCommand(yargs) {
+    .command('compare', 'Compare build sizes using output from parse command', function compareCommand(yargs) {
         let argv = yargs
             .usage('Usage: $0 compare <previous> <new> [options]')
+            .example('$0 compare ./previous.json ./new.json', 'Compare two sets of build sizes and output results as Markdown')
+            .example('$0 compare ./previous.json ./new.json --format=json', 'Compare two sets of build sizes and output results as JSON')
             .positional('previous', {
-                describe: 'File with parse results of previous build',
+                describe: 'File with parse results of previous build i.e. output from parse command',
                 normalize: true,
                 type: 'string'
             })
             .positional('new', {
-                describe: 'File with parse results of new build',
+                describe: 'File with parse results of new build i.e. output from parse command',
                 normalize: true,
                 type: 'string'
             })
